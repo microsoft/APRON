e0cb02e2a0759c4f8d94a144c00176a37dd94e4c
Sangho Lee <Sangho.Lee@microsoft.com>
2023-05-12 10:11:05 -0700

    add kernel module source files and build pipeline scripts

---
+++ .pipelines/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# Copyright (c) 2023 Microsoft Corporation.
+# Licensed under the MIT License.
+
+sudo apt update
+sudo apt install -y \
+	wget \
+	build-essential \
+	libncurses-dev \
+	bison \
+	flex \
+	libssl-dev \
+	libelf-dev
